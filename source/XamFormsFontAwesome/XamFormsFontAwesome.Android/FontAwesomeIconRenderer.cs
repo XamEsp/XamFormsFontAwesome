81548a206e29c645f427df30f803735c6e07917b
rodrigojuarez <rodrigomjuarez@gmail.com>
2019-02-10 20:52:13 -0300

    First version tested on Android

---
+++ source/XamFormsFontAwesome/XamFormsFontAwesome.Android/FontAwesomeIconRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamFormsFontAwesome;
+using XamFormsFontAwesome.Droid;
+
+[assembly: ExportRenderer(typeof(FontAwesomeIcon), typeof(FontAwesomeIconRenderer))]
+namespace XamFormsFontAwesome.Droid
+{
+   public class FontAwesomeIconRenderer : LabelRenderer
+   {
+      protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+      {
+         base.OnElementChanged(e);
+         if (e.OldElement == null)
+         {
+            //The ttf in /Assets is CaseSensitive, so name it FontAwesome.ttf
+            Control.Typeface = Typeface.CreateFromAsset(Forms.Context.Assets, FontAwesomeIcon.Typeface + ".ttf");
+         }
+      }
+   }
+}
---
+++ source/XamFormsFontAwesome/XamFormsFontAwesome/FontAwesomeIcon.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace XamFormsFontAwesome
+{
+   public class FontAwesomeIcon : Label
+   {
+      //Must match the exact "Name" of the font which you can get by double clicking the TTF in Windows
+      // Android
+      // FontAwesome
+      // IOS
+      // FontAwesome5FreeSolid
+
+      public static string Typeface => Device.RuntimePlatform == Device.Android ? "fa-solid-900" : "FontAwesome5FreeSolid";
+
+      public FontAwesomeIcon()
+      {
+         FontFamily = Typeface;
+      }
+
+      public FontAwesomeIcon(string fontAwesomeIcon = null)
+      {
+         FontFamily = Typeface; //iOS is happy with this, Android needs a renderer to add ".ttf"
+         Text = fontAwesomeIcon;
+      }
+   }
+}
